6b23c88a8c87ff9ec689a956e117605b31be4e3b
APS229 <apsofficial229@gmail.com>
2022-03-15 12:18:37 +0530

    Add config file

---
+++ config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+exports.token = 'NDQxNTc2MzM1MTY1NjIwMjI0.WusAAg.Ao66mHOBXFd89xDozOuLDleDqyo';
+
+exports.username = 'Qt Bot';
+
+exports.id = '441576335165620224';
+
+exports.cmdchar = '.';
+
+exports.avatarURL = 'https://cdn.discordapp.com/avatars/441576335165620224/272b75674d0a71edfebf5197878d74be.png?size=2048';
+
+exports.developers = ['427822675176718346']; // APS
+
+exports.youtubeapi = 'AIzaSyAIJEdrBqDgOkzpp_FDLpfwzJfHLlchr_Y';
+
+exports.activity = 'PLAYING';
