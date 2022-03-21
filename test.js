274050c02f1dc01d0ece7ea2a9b6d5376bb5ca08
APS229 <apsofficial229@gmail.com>
2022-03-21 17:17:43 +0530

    Testing

--- test.js
+++ test.js
@@ -2,8 +2,4 @@ console.log(process.env.DATABASE_URL);
 
 const { Client } = require('pg')
 const client = new Client({user: 'postgres'})
-client.connect(process.env.DATABASE_URL)
-client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-  console.log(err ? err.stack : res.rows[0].message) // Hello World!
-  client.end()
-})
+client.connect(process.env.DATABASE_URL).then(() => console.log("Connected")).catch(console.log);
