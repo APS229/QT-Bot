03a232c5e197af08cb53c98fe195f2fd01984792
APS229 <apsofficial229@gmail.com>
2022-03-23 22:23:43 +0530

    Testing database/xp

--- commands/profile.js
+++ commands/profile.js
@@ -7,7 +7,7 @@ const commands = {
         execute(target, channel, user, server, client) {
             Database.query(`select * from profile where id='${user.id}'`).then(res => {
                 if (res.rows.length) channel.say(`Your level: ${res.rows[0].level}\nYour XP: ${res.rows[0].xp}\nYour balance: ${res.rows[0].balance}`);
-                else channel.say("You're not in database.");
+                else channel.say("You're not in database. Send some messages to gain XP!");
             });
         }
     }
