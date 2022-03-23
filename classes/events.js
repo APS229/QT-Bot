6a84305800b8a60636a1b2bb689bc9927ba0f068
APS229 <apsofficial229@gmail.com>
2022-03-23 15:23:19 +0530

    Testing xp

--- classes/events.js
+++ classes/events.js
@@ -126,19 +126,19 @@ class Events {
 
             if (!Config.excludedCh.includes(channel.name)) {
                 Database.query('select * from profile where id = ' + user.id, (err, res) => {
-                    if (err) {
+                    if (err) return console.log(err);
+                    if (res.rows.length) {
+                        const xp = res.rows[0].xp + 10;
+                        Database.query(`update profile set xp = ${xp} where id = ${user.id}`, (err, res) => {
+                            if (err) return console.log(err);
+                            channel.say(res.rows[0].xp);
+                        });
+                    }
+                    else {
                         Database.query(`insert into profile values('${user.id}', 1, 0, 0)`, (err, res) => {
                             if (err) return console.log(err);
                             channel.say(res.rows[0]);
                         });
-                    }
-                    else {
-                        console.log(res);
-                        // const xp = res.rows[0].xp + 10;
-                        // Database.query(`update profile set xp = ${xp} where id = ${user.id}`, (err, res) => {
-                        //     if (err) return console.log(err);
-                        //     channel.say(res.rows[0].xp);
-                        // });
                     }
 
                 });
