28ad6562dada0aa2cc64ae055c917e9872b76de6
APS229 <apsofficial229@gmail.com>
2022-05-23 14:48:36 +0530

    Fix rob event bug

--- classes/events.js
+++ classes/events.js
@@ -48,7 +48,8 @@ class Events {
             if (channel.type === 'text' && user.guild && user.id === user.guild.ownerID) user.owner = true;
             if (server.id === '978201529397948466' && user.id === '270904126974590976' && discord.type === 'REPLY') {
                 let points = 0;
-                const id = channel.messages.fetch(discord.reference.messageId).author.id;
+                const userMessage = await channel.messages.fetch(discord.reference.messageId);
+                const id = userMessage.author.id;
                 if (message.includes("TINY portion")) {
                     points = 1;
                 }
