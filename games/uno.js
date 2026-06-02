12017fa3ebe18c46d1609da9453e989797f9ca66
APS229 <apsofficial229@gmail.com>
2026-06-02 11:24:39 +0530

    UNO: Add UNO! interaction & Fix reverse

--- games/uno.js
+++ games/uno.js
@@ -14,9 +14,10 @@ class UNO extends Games.Game {
         this.firstTurn = true;
         this.actions = "";
         this.queue = [];
+        this.prevPlayer = '';
         this.topCard = '';
         this.topCardColor = '';
-        this.playerTime = 15;
+        this.playerTime = 30;
         this.playerTimer = null;
         this.init();
     }
@@ -49,7 +50,7 @@ class UNO extends Games.Game {
     async onStart() {
         super.onStart();
         for (const player of this.players.keys()) {
-            this.players.set(player, { uno: false, cards: [] });
+            this.players.set(player, { uno: false, hand: [] });
             this.queue.push(player);
         }
         this.queue = this.queue.shuffle();
@@ -59,7 +60,7 @@ class UNO extends Games.Game {
     }
     assignCards() {
         for (const player of this.players.keys()) {
-            this.drawCards(player, 7);
+            this.drawCards(player, 2);
         }
     }
     format(card) {
@@ -83,12 +84,16 @@ class UNO extends Games.Game {
     }
     async onNextRound() {
         if (!this.started) return;
+        if (this.playerTimer) clearTimeout(this.playerTimer);
         if (this.firstTurn) {
             let card = this.deck.random();
             this.topCard = card;
-            if (card.startsWith('wild')) card += ` ${colors.random()}`;
-            this.topCardColor = this.playCard(card);
+            if (card.startsWith('wild')) {
+                this.topCardColor = colors.random();
+                card += ` ${this.topCardColor}`;
+            }
             this.channel.send(`The top card is: **${this.format(card)}**`);
+            this.playCard(card);
             this.firstTurn = false;
         }
 
@@ -112,58 +117,78 @@ class UNO extends Games.Game {
         this.play(interaction, 'selectmenu');
     }
     play(interaction, interactionType) {
-        const currentPlayer = this.queue[0];
         const player = interaction.member.id;
-        if (currentPlayer !== player) return interaction.reply({ content: "It is currently not your turn.", flags: 'Ephemeral' });
+        if (player !== this.queue[0]) return interaction.reply({ content: "It is currently not your turn.", flags: 'Ephemeral' });
 
         // TODO: check for button / select menu
-        if (!interaction.content && !interaction.isChatInputCommand()) return interaction.reply({content: "Play button/menu is currently work-in-progress. Use `/play card: [card]` instead."});
+        if (!interaction.content && !interaction.isChatInputCommand()) return interaction.reply({ content: "Play button/menu is currently work-in-progress. Use `/play card: [card]` instead." });
+
         let card = (interaction.content?.split(' ').slice(1).join(' ') || interaction.options?.get('card').value).toLowerCase();
 
         const [cardName, cardAction, cardActionColor] = card.split(' ');
+        const playerHand = this.players.get(player).hand;
 
         if (cardName === 'wild') {
             if (cardAction === '+4') {
-                if (!this.players.get(player).cards.includes('wild +4')) return interaction.reply({ content: `You don't have the card: ${this.format('wild +4')}`, flags: 'Ephemeral' });
+                if (!playerHand.includes('wild +4')) return interaction.reply({ content: `You don't have the card: ${this.format('wild +4')}`, flags: 'Ephemeral' });
                 if (!colors.includes(cardActionColor)) return interaction.reply({ content: "Please specify a color after Wild +4.", flags: 'Ephemeral' });
                 else card = 'wild +4';
             }
-            else if (!this.players.get(player).cards.includes('wild')) return interaction.reply({ content: `You don't have the card: ${this.format('wild')}`, flags: 'Ephemeral' });
+            else if (!playerHand.includes('wild')) return interaction.reply({ content: `You don't have the card: ${this.format('wild')}`, flags: 'Ephemeral' });
             else if (!colors.includes(cardAction)) return interaction.reply({ content: "Please specify a color after Wild.", flag: 'Ephemeral' });
             else card = 'wild';
         }
         else if (!Client.CARDS.includes(card)) return interaction.reply({ content: "That card doesn't exist in this game.", flags: 'Ephemeral' });
-        else if (!this.players.get(player).cards.includes(card)) return interaction.reply({ content: `You don't have the card: ${this.format(card)}`, flags: 'Ephemeral' });
+        else if (!playerHand.includes(card)) return interaction.reply({ content: `You don't have the card: ${this.format(card)}`, flags: 'Ephemeral' });
         else if (colors.includes(cardName)) {
             if (cardName !== this.topCardColor && cardAction !== this.topCard.split(' ')[1]) return interaction.reply({ content: "Your card must either match the color or the action of the top card!", flags: 'Ephemeral' });
         }
+        if (this.playerTimer) clearTimeout(this.playerTimer);
+
+        if (this.prevPlayer) {
+            const prevPlayerData = this.players.get(this.prevPlayer);
+            if (prevPlayerData.hand.length === 1 && !prevPlayerData.uno) {
+                this.channel.send(`<@${this.prevPlayer}> forgot to say UNO! and drew a card.`);
+                this.drawCards(this.prevPlayer, 1);
+            }
+        }
+
         this.topCard = card;
-        this.topCardColor = this.playCard(`${cardName} ${cardAction} ${cardActionColor}`, interaction.member.id);
+        this.playCard(`${cardName} ${cardAction} ${cardActionColor}`, player);
         interaction.reply({ content: `You played: ${this.format(card)}`, flags: 'Ephemeral' });
 
-        if (this.playerTimer) clearTimeout(this.playerTimer);
+
+        if (!playerHand.length) {
+            this.winner = player;
+            return this.onEnd();
+        }
         this.onNextRound();
     }
     draw(interaction) {
         const player = interaction.member.id;
         if (this.queue[0] !== player) return interaction.reply({ content: "It is currently not your turn.", flags: 'Ephemeral' });
 
+        if (this.playerTimer) clearTimeout(this.playerTimer);
         const card = this.drawCards(player, 1)[0];
-        interaction.reply({ content: `You have drawn: ${this.format(card)}`, flags: 'Ephemeral' });
+        if (!interaction.content) interaction.reply({ content: `You have drawn: ${this.format(card)}`, flags: 'Ephemeral' });
         this.channel.send(`<@${player}> drew a card.`);
         this.players.get(player).uno = false;
         this.queue.shift();
         this.queue.push(player);
 
-        if (this.playerTimer) clearTimeout(this.playerTimer);
         this.onNextRound();
     }
     showHand(interaction) {
-        const cards = this.players.get(interaction.member.id).cards.map(card => this.format(card));
+        const cards = this.players.get(interaction.member.id).hand.map(card => this.format(card));
         return interaction.reply({ content: `Your current hand: ${cards.join(', ')}`, flags: 'Ephemeral' });
     }
     declareUno(interaction) {
-        // TODO: implement
+        const playerData = this.players.get(interaction.member.id);
+        if (playerData.hand.length === 1 && !playerData.uno) {
+            playerData.uno = true;
+            this.channel.send(`UNO! <@${interaction.member.id}> has 1 card left.`);
+        }
+        if (!interaction.content) interaction.deferUpdate();
     }
     playCard(fullCardName) {
         if (!this.started) return;
@@ -172,27 +197,28 @@ class UNO extends Games.Game {
         const nextPlayer = player ? this.queue[1] : this.queue[0];
 
         const [cardName, cardAction, cardActionColor] = fullCardName.split(' ');
-        let cardColor = '';
 
         // First turn means the card should be put into discard pile from the deck
         const card = `${cardName}` + (cardName === 'wild' && cardAction !== '+4' ? '' : ` ${cardAction}`);
-        const index = player ? this.players.get(player).cards.indexOf(card) : this.deck.indexOf(card);
-        if (index > -1) player ? this.players.get(player).cards.splice(index, 1) : this.deck.splice(index, 1);
+        const index = player ? this.players.get(player).hand.indexOf(card) : this.deck.indexOf(card);
+        if (index > -1) player ? this.players.get(player).hand.splice(index, 1) : this.deck.splice(index, 1);
         this.discardPile.push(card);
 
-        if (player) {
+        // Array.reverse automatically puts the current player in last spot
+        if (player && cardAction !== 'reverse') {
             this.queue.shift();
             this.queue.push(player);
+            this.prevPlayer = player;
         }
 
         if (cardName === 'wild') {
-            if (colors.includes(cardAction)) cardColor = cardAction;
+            if (colors.includes(cardAction)) this.topCardColor = cardAction;
             else if (cardAction === '+4') {
                 this.actions += `<@${nextPlayer}> was forced to draw 4 cards.`;
                 this.drawCards(nextPlayer, 4);
                 this.queue.shift();
                 this.queue.push(nextPlayer);
-                cardColor = cardActionColor;
+                this.topCardColor = cardActionColor;
             }
         }
         else if (colors.includes(cardName)) {
@@ -211,9 +237,8 @@ class UNO extends Games.Game {
                 this.queue.reverse();
                 this.actions += "The turn order was reversed.";
             }
-            cardColor = cardName;
+            this.topCardColor = cardName;
         }
-        return cardColor;
     }
     drawCards(player, amount) {
         if (!this.started) return;
@@ -230,11 +255,11 @@ class UNO extends Games.Game {
 
             drawnCards.push(drawnCard);
         }
-        this.players.get(player).cards.push(...drawnCards);
+        this.players.get(player).hand.push(...drawnCards);
         return drawnCards;
     }
     onLeave(userId) {
-        const cards = this.started ? this.players.get(userId).cards : [];
+        const cards = this.started ? this.players.get(userId).hand : [];
         super.onLeave(userId);
         if (this.started) {
             if (this.players.size < 2) {
@@ -257,7 +282,7 @@ class UNO extends Games.Game {
 
         const topCardColor = this.topCardColor;
         const img = new AttachmentBuilder(`./images/${this.id}/colors/${topCardColor}.png`);
-        const players = this.queue.map(player => `<@${player}> (${this.players.get(player).cards.length})`).join('\n');
+        const players = this.queue.map(player => `<@${player}> (${this.players.get(player).hand.length})`).join('\n');
         const embed = new EmbedBuilder()
             .setColor(Tools.toTitleCase(topCardColor))
             .setTitle(this.name)
@@ -284,7 +309,8 @@ class UNO extends Games.Game {
                 new ButtonBuilder()
                     .setCustomId('play')
                     .setLabel('Play')
-                    .setStyle('Primary'),
+                    .setStyle('Primary')
+                    .setDisabled(true),
                 new ButtonBuilder()
                     .setCustomId('hand')
                     .setLabel('Hand')
